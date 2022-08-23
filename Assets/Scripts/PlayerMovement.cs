7f69b95ac718a1a3628ecae786d47839cf539b68
Chichou <ch1ch0ud3v@gmail.com>
2022-08-23 16:33:35 +0200

    input configuration
    creation of PlayerMovement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector2 direction = Vector2.zero;
-    [SerializeField] float _speed =5f;
-    //[SerializeField] bool isJumping = false;
-    //[SerializeField] bool isWalking = false;
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            direction = Vector2.right;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            direction = Vector2.left;
-        }
-
-        transform.Translate(direction * _speed * Time.deltaTime);
-        direction = Vector2.zero;
-        
-    }
+    [SerializeField] InputActionReference _moveInput;
+    [SerializeField] InputActionReference _jumpInput;
+    [SerializeField] InputActionReference _actionButtonInput;
+    [SerializeField] InputActionReference _fightInput;
 }
